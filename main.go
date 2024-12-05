f32ac949528005b3ffb112b88a449be7c0a55f9b
kkbs <men6586@yandex.ru>
2024-12-05 23:05:10 +0300

    first

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func main() {
+
+	servMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: servMux,
+	}
+	server.ListenAndServe()
+}
