277d4e05b8d2883326ced8075ebb0f546dc4bc19
kkbs <men6586@yandex.ru>
2024-12-10 21:21:18 +0300

    change

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "net/http"
 
+//type apiHandler struct{}
+//
+//func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {
+//
+//}
+
 func main() {
 
 	servMux := http.NewServeMux()
@@ -9,5 +15,8 @@ func main() {
 		Addr:    ":8080",
 		Handler: servMux,
 	}
+	servMux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	servMux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server.ListenAndServe()
 }
